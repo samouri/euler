989706a28a13b50d1956a1e1b7fa9332359479d8
Jake F - @samouri <samouri@users.noreply.github.com>
2017-10-08 20:50:15 -0400

    problem 4 :)

--- solutions.ml
+++ solutions.ml
@@ -7,13 +7,14 @@ open Core;;
 let rec sum ?(multiple=[1]) = function
   | 0 -> 0
   | n -> (sum ~multiple (n-1)) + if List.exists ~f: (fun y -> n mod y = 0 ) multiple then n else 0
-  ;;
+;;
 
 (*
   a worse version of sum because of mem usage and speed but at least i got to
   play with some cool functions like reverse applicative operator
 *)
-let sum2 ?(multiple=[1]) n = List.range 0 (n+1)
+let sum2 ?(multiple=[1]) n = 
+  List.range 0 (n+1)
   |> List.filter ~f: (fun z -> (List.exists ~f: (fun y -> z mod y = 0) multiple ) )
   |> List.fold ~f: (+) ~init: 0;;
 
@@ -21,7 +22,7 @@ let rec fib = function
   | 0 -> 1
   | 1 -> 2
   | n -> fib (n - 1) + fib (n - 2)
-  ;;
+;;
 
 (*
   Keeping this for old times sake so i see just how bad my first solution
@@ -39,23 +40,61 @@ let rec pfactors ?(factors=[]) ?(divisor=2) n =
   if n = 1 then List.dedup factors 
   else if n mod divisor = 0 then pfactors ~factors:(divisor::factors) ~divisor (n/divisor)
   else pfactors ~factors ~divisor:(divisor+1) n;;
-    
-(*
-let prob1 = sum ~multiple:[3; 5] 999;;
-print_endline ( "problem 1: " ^ ( string_of_int prob1 ) );;
 
-let prob1v2 = sum2 ~multiple:[3; 5] 999;;
-print_endline ( "problem 1v2: " ^ ( string_of_int prob1v2 ) );;
+(* problem 1
+*)
+let prob1 =  fun() ->
+  let answer = sum ~multiple:[3; 5] 999 in
+  print_endline ( "problem 1: " ^ ( string_of_int answer ) )
+;;
+
+(* problem 2
+*)
+let prob2 = fun() -> 
+  let answer = sum_until_4mil 0 in
+  print_endline ( "problem 2: " ^ ( string_of_int answer ) )
+;;
+
+let prob3 = fun() ->
+  let answer = match List.max_elt (pfactors 600851475143 ) (-) with
+    | Some x -> x
+    | None -> 0 in
+  print_endline ( "problem 3: " ^ ( string_of_int answer ) )
+;;
+
+
+(* problem 4 
+ * A palindromic number reads the same both ways. 
+ * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+*)
+let rec isPalindromic = function
+  | "" -> true
+  | s when (String.length s) = 0 -> true
+  | s -> (String.get s 0) = (String.get s ((String.length s)-1)) &&
+         isPalindromic (String.slice s 1 ( String.length s -1) )
+;; 
+let isPalindromicNumber n = string_of_int n |> isPalindromic;;
+
+let prob4 = fun() ->
+  let maxPalindrome = ref 0 in
+  for i = 100 to 999 do
+    for j = 100 to 999 do
+      let product = i * j in
+      if (isPalindromicNumber product) && product > !maxPalindrome then
+        maxPalindrome := product
+    done
+  done;
+  print_endline ("problem 4: " ^ string_of_int !maxPalindrome);
+;;
+
+(* problem 5
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. 
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 *)
 
-(*
-let prob2 = sum_until_4mil 0;;
-print_endline ( "problem 2: " ^ ( string_of_int prob2 ) );;
-*)
 
-(*
-let prob3 = match List.max_elt (pfactors 600851475143 ) (-) with
- | Some x -> x
- | None -> 0;;
-print_endline ( "problem 3: " ^ ( string_of_int prob3 ) ) ;;
-*)
+prob1();
+prob2();
+prob3();
+prob4();
